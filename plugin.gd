26397c2432c454064b8db45fd7027a46c8890d18
addmix <addmixtaylor@gmail.com>
2023-01-15 02:18:32 -0700

    Update plugin.gd

--- plugin.gd
+++ plugin.gd
@@ -6,8 +6,8 @@ var base_dir : String = "res://addons/godot_aerodynamic_physics/"
 
 func _enter_tree():
 	SettingsUtils.ifndef("physics/3d/aerodynamics/substeps", 1)
-	add_custom_type("AeroSurface", "Node3D", load(base_dir + "core/aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("AeroBody", "RigidDynamicBody3D", load(base_dir + "core/aero_body_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("AeroSurface3D", "Node3D", load(base_dir + "core/aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("AeroBody3D", "RigidBody3D", load(base_dir + "core/aero_body_3d.gd"), load(base_dir + "icons/node3d.svg"))
 
 	#register units singleton
 	add_autoload_singleton("AeroUnits", base_dir + "core/singletons/units.gd")

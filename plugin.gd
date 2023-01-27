b3db2393bea523002fabd4aa41bb72c12a0ecfdb
addmix <addmixtaylor@gmail.com>
2023-01-26 19:19:04 -0700

    fixed plugin custom type removal.

--- plugin.gd
+++ plugin.gd
@@ -13,13 +13,16 @@ func _enter_tree():
 	add_custom_type("ProceduralAeroSurface3D", "AeroSurface3D", load(base_dir + "core/procedural_aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
 	add_custom_type("ProceduralAeroSurfaceConfig", "ProceduralAeroSurfaceConfig", load(base_dir + "core/procedural_aero_surface_config.gd"), load(base_dir + "icons/node3d.svg"))
 
-
-	#register units singleton
 	add_autoload_singleton("AeroUnits", base_dir + "core/singletons/units.gd")
 	add_node_3d_gizmo_plugin(gizmo_plugin)
 
 func _exit_tree():
-	remove_custom_type("AeroSurface")
-	remove_custom_type("AeroBody")
+	remove_custom_type("AeroBody3D")
+	remove_custom_type("AeroSurface3D")
+	remove_custom_type("ManualAeroSurface3D")
+	remove_custom_type("ManualAeroSurfaceConfig")
+	remove_custom_type("ProceduralAeroSurface3D")
+	remove_custom_type("ProceduralAeroSurfaceConfig")
 
+	remove_autoload_singleton("AeroUnits")
 	remove_node_3d_gizmo_plugin(gizmo_plugin)

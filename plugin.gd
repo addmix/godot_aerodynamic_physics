e2d638c7754a91242079d69cecfab27bfa267569
addmix <addmixtaylor@gmail.com>
2023-01-26 19:37:48 -0700

    added default node icons and improved plugin loading convention

--- plugin.gd
+++ plugin.gd
@@ -1,20 +1,36 @@
 @tool
 extends EditorPlugin
+#icons
+const node_icon = preload("./icons/node.svg")
+const node2d_icon = preload("./icons/node2d.svg")
+const node3d_icon = preload("./icons/node3d.svg")
 
-var gizmo_plugin : EditorNode3DGizmoPlugin = preload("./core/spatial_gizmo/aero_surface_gizmo.gd").new()
-var base_dir : String = "res://addons/godot_aerodynamic_physics/"
+#plugin gizmos
+const gizmo_plugin = preload("./core/spatial_gizmo/aero_surface_gizmo.gd")
+var gizmo_plugin_instance = gizmo_plugin.new()
+
+#singletons
+const aero_units = "./core/singletons/units.gd"
+
+#nodes
+const aero_body_3d = preload("./core/aero_body_3d.gd")
+const aero_surface_3d = preload("./core/aero_surface_3d.gd")
+const manual_aero_surface_3d = preload("./core/manual_aero_surface_3d.gd")
+const manual_aero_surface_config = preload("./core/manual_aero_surface_config.gd")
+const procedural_aero_surface_3d = preload("./core/procedural_aero_surface_3d.gd")
+const procedural_aero_surface_config = preload("./core/procedural_aero_surface_config.gd")
 
 func _enter_tree():
 	SettingsUtils.ifndef("physics/3d/aerodynamics/substeps", 1)
-	add_custom_type("AeroBody3D", "RigidBody3D", load(base_dir + "core/aero_body_3d.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("AeroSurface3D", "Node3D", load(base_dir + "core/aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("ManualAeroSurface3D", "AeroSurface3D", load(base_dir + "core/manual_aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("ManualAeroSurfaceConfig", "ManualAeroSurfaceConfig", load(base_dir + "core/manual_aero_surface_config.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("ProceduralAeroSurface3D", "AeroSurface3D", load(base_dir + "core/procedural_aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
-	add_custom_type("ProceduralAeroSurfaceConfig", "ProceduralAeroSurfaceConfig", load(base_dir + "core/procedural_aero_surface_config.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("AeroBody3D", "RigidBody3D", aero_body_3d, node3d_icon)
+	add_custom_type("AeroSurface3D", "Node3D", aero_surface_3d, node3d_icon)
+	add_custom_type("ManualAeroSurface3D", "AeroSurface3D", manual_aero_surface_3d, node3d_icon)
+	add_custom_type("ManualAeroSurfaceConfig", "ManualAeroSurfaceConfig", manual_aero_surface_config, node3d_icon)
+	add_custom_type("ProceduralAeroSurface3D", "AeroSurface3D", procedural_aero_surface_3d, node3d_icon)
+	add_custom_type("ProceduralAeroSurfaceConfig", "ProceduralAeroSurfaceConfig", procedural_aero_surface_config, node3d_icon)
 
-	add_autoload_singleton("AeroUnits", base_dir + "core/singletons/units.gd")
-	add_node_3d_gizmo_plugin(gizmo_plugin)
+	add_autoload_singleton("AeroUnits", aero_units)
+	add_node_3d_gizmo_plugin(gizmo_plugin_instance)
 
 func _exit_tree():
 	remove_custom_type("AeroBody3D")
@@ -25,4 +41,4 @@ func _exit_tree():
 	remove_custom_type("ProceduralAeroSurfaceConfig")
 
 	remove_autoload_singleton("AeroUnits")
-	remove_node_3d_gizmo_plugin(gizmo_plugin)
+	remove_node_3d_gizmo_plugin(gizmo_plugin_instance)

aaf0892fdbcfff404dc7a288ee73a703a8beeeb2
addmix <addmixtaylor@gmail.com>
2023-01-26 19:15:59 -0700

    fixed plugin custom type initialization.

--- plugin.gd
+++ plugin.gd
@@ -6,18 +6,16 @@ var base_dir : String = "res://addons/godot_aerodynamic_physics/"
 
 func _enter_tree():
 	SettingsUtils.ifndef("physics/3d/aerodynamics/substeps", 1)
-	add_custom_type("AeroSurface3D", "Node3D", load(base_dir + "core/aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
 	add_custom_type("AeroBody3D", "RigidBody3D", load(base_dir + "core/aero_body_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("AeroSurface3D", "Node3D", load(base_dir + "core/aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("ManualAeroSurface3D", "AeroSurface3D", load(base_dir + "core/manual_aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("ManualAeroSurfaceConfig", "ManualAeroSurfaceConfig", load(base_dir + "core/manual_aero_surface_config.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("ProceduralAeroSurface3D", "AeroSurface3D", load(base_dir + "core/procedural_aero_surface_3d.gd"), load(base_dir + "icons/node3d.svg"))
+	add_custom_type("ProceduralAeroSurfaceConfig", "ProceduralAeroSurfaceConfig", load(base_dir + "core/procedural_aero_surface_config.gd"), load(base_dir + "icons/node3d.svg"))
+
 
 	#register units singleton
 	add_autoload_singleton("AeroUnits", base_dir + "core/singletons/units.gd")
-
-#	print(InputMap.has_action("toggle_aerodynamic_debug"))
-#	InputMap.add_action("toggle_aerodynamic_debug")
-#	var debug_key := InputEventKey.new()
-#	debug_key.set_keycode(KEY_F10)
-#	InputMap.action_add_event("toggle_aerodynamic_debug", debug_key)
-
 	add_node_3d_gizmo_plugin(gizmo_plugin)
 
 func _exit_tree():

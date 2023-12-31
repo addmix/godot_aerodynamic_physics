e61dece2195da9c516c58709c8890455fa357c6c
addmix <38119387+addmix@users.noreply.github.com>
2023-12-31 04:01:46 -0700

    Made AeroRotator3D functional

--- core/aero_influencer_3d/aero_rotator_3d/aero_rotator_3d.gd
+++ core/aero_influencer_3d/aero_rotator_3d/aero_rotator_3d.gd
@@ -13,7 +13,7 @@ func _ready():
 			aero_influencers.append(i)
 
 func _calculate_forces(_world_air_velocity : Vector3, _air_density : float, _relative_position : Vector3, _altitude : float, substep_delta : float = 0.0) -> PackedVector3Array:
-	super._calculate_forces(_world_air_velocity, _air_density, _relative_position, _altitude)
+	super._calculate_forces(_world_air_velocity, _air_density, _relative_position, _altitude, substep_delta)
 	
 	if not is_equal_approx(angular_velocity.length_squared(), 0.0):
 		#rotate by angular velocity
@@ -25,15 +25,12 @@ func _calculate_forces(_world_air_velocity : Vector3, _air_density : float, _rel
 	for influencer : AeroInfluencer3D in aero_influencers:
 		#position relative to AeroBody origin, using global rotation
 		var influencer_relative_position : Vector3 = (global_transform.basis * influencer.position) + relative_position
-		var force_and_torque : PackedVector3Array = influencer._calculate_forces(world_air_velocity + (-angular_velocity).cross(influencer_relative_position), air_density, relative_position, position.y, substep_delta)
+		var force_and_torque : PackedVector3Array = influencer._calculate_forces(world_air_velocity + (-angular_velocity).cross(influencer_relative_position), air_density, influencer_relative_position, position.y, substep_delta)
 		
 		force += force_and_torque[0]
 		torque += force_and_torque[1]
 	
 	_current_force = force
 	_current_torque = torque
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		print(force, torque)
 	
 	return PackedVector3Array([force, torque])

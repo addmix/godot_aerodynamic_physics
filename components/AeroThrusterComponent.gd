7b069ee0aa6b82ebf41f201c7fe19307b544dc61
addmix <38119387+addmix@users.noreply.github.com>
2024-06-20 01:31:55 -0600

    Added AeroThrusterComponent

---
+++ components/AeroThrusterComponent.gd
@@ -0,0 +1,64 @@
+@icon("../icons/JetThrusterComponent.svg")
+extends Marker3D
+class_name AeroThrusterComponent
+
+
+const AeroNodeUtils = preload("../utils/node_utils.gd")
+
+@onready var rigid_body : RigidBody3D = AeroNodeUtils.get_first_parent_of_type(self, RigidBody3D)
+
+@export var enabled : bool = true
+@export_group("Control")
+@export var get_throttle_from_aero_body : bool = true
+@export var throttle : float = 1.0
+
+@export_group("Simulation Parameters")
+@export var intake_area : float = 0.5
+@export var intake_fan_max_velocity : float = 200.0
+@export var max_exhaust_velocity : float = 300
+#@export var exit_area : float = 1.0
+
+#fuel
+@export var max_fuel_flow : float = 1.0 #flow rate in kilograms per second
+#@export var fuel_expansion_ratio : float = 10.0
+
+
+func _physics_process(delta : float) -> void:
+	if not enabled:
+		return
+	
+	var force_magnitude : float = calculate_force()
+	
+	if rigid_body:
+		rigid_body.apply_force(-global_transform.basis.z * force_magnitude, rigid_body.global_transform.basis * position)
+	
+	if rigid_body is AeroBody3D and get_throttle_from_aero_body:
+		throttle = rigid_body.throttle_command
+
+func calculate_force() -> float:
+	var altitude : float = 0.0
+	var air_velocity : Vector3 = rigid_body.linear_velocity
+	if rigid_body is AeroBody3D:
+		altitude = rigid_body.altitude
+		air_velocity = -rigid_body.air_velocity
+	else:
+		altitude = AeroUnits.get_altitude(self)
+	
+	var intake_air_velocity : float = air_velocity.dot(-global_basis.z)
+	var intake_air_density : float = AeroUnits.get_density_at_altitude(altitude)
+	var intake_air_pressure : float = AeroUnits.get_pressure_at_altitude(altitude)
+	var intake_mass_flow_rate : float = intake_air_density * intake_air_velocity * intake_area
+	
+	var fuel_burn_rate : float = max_fuel_flow * throttle
+	var exhaust_velocity : float = calculate_exhaust_velocity()
+	#https://www.omnicalculator.com/physics/ideal-gas-law
+	#var exhaust_pressure : float = 0.0 #will need to use fuel combustion expansion ratio
+	var exhaust_mass_flow_rate : float = intake_mass_flow_rate + (intake_air_density * intake_fan_max_velocity * throttle * intake_area) + fuel_burn_rate
+	
+	
+	#mass_flow_rate = mass/time
+	#mass_flow_rate == density * velocity * area
+	return (exhaust_mass_flow_rate * exhaust_velocity) - (intake_mass_flow_rate * intake_air_velocity)# + (exhaust_pressure - intake_air_pressure) * exit_area
+
+func calculate_exhaust_velocity() -> float:
+	return max_exhaust_velocity * throttle

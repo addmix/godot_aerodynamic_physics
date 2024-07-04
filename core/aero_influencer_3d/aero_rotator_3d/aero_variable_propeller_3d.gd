d8702bf8c5c2d972a39fcded69136324056e53af
addmix <38119387+addmix@users.noreply.github.com>
2024-07-03 19:59:32 -0600

    Fix AeroVariablePropeller3D pitch/collective not being applied properly.

--- core/aero_influencer_3d/aero_rotator_3d/aero_variable_propeller_3d.gd
+++ core/aero_influencer_3d/aero_rotator_3d/aero_variable_propeller_3d.gd
@@ -18,6 +18,7 @@ func _update_transform_substep(substep_delta : float) -> void:
 	
 	for influencer : AeroInfluencer3D in propeller_instances:
 		influencer.default_transform.basis = Basis.from_euler(Vector3(deg_to_rad(propeller_pitch), influencer.default_transform.basis.get_euler().y, influencer.default_transform.basis.get_euler().z)) 
+		influencer.transform = influencer.default_transform
 
 func is_overriding_body_sleep() -> bool:
 	return super.is_overriding_body_sleep() and not is_equal_approx(propeller_pitch, 0.0)

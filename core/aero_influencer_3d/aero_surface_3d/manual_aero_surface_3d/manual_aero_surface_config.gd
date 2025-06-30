7f8a3cb51b6f7dc75de000c8d81a1820d2916003
addmix <38119387+addmix@users.noreply.github.com>
2025-06-30 15:06:30 -0600

    Adjust default curves to make use of min/max domain

--- core/aero_influencer_3d/aero_surface_3d/manual_aero_surface_3d/manual_aero_surface_config.gd
+++ core/aero_influencer_3d/aero_surface_3d/manual_aero_surface_3d/manual_aero_surface_config.gd
@@ -17,19 +17,20 @@ class_name ManualAeroSurfaceConfig
 ##Multiplier curve that modifies drag depending on the sweep angle of the AeroSurface3D.
 @export var sweep_drag_multiplier_curve : Curve = preload("../../../resources/default_sweep_drag_multiplier.tres").duplicate()
 func get_drag_at_sweep_angle(sweep_angle : float) -> float:
-	return sweep_drag_multiplier_curve.sample_baked(abs(sweep_angle / PI))
+	return sweep_drag_multiplier_curve.sample_baked(abs(remap(sweep_angle, -PI, PI, sweep_drag_multiplier_curve.min_domain, sweep_drag_multiplier_curve.max_domain)))
 ##Multiplier curve that modifies drag depending on the AeroBody3D's Mach number.
 @export var drag_at_mach_multiplier_curve : Curve = preload("../../../resources/default_drag_at_mach_curve.tres").duplicate()
 func get_drag_multiplier_at_mach(mach : float) -> float:
-	return drag_at_mach_multiplier_curve.sample_baked(mach / 10.0)
+	return drag_at_mach_multiplier_curve.sample_baked(mach)
 ##Evaluation curve for turbulent movements based on AeroSurface3D's angle of attack. (Unused)
 #@export var buffet_aoa_curve : Curve #unused
 
 const MathUtils = preload("../../../../utils/math_utils.gd")
 
 func get_lift_coefficient(aoa : float) -> float:
-	var sample : float = lift_aoa_curve.sample_baked(aoa / PI / 2.0 + 0.5)
-	return sample * MathUtils.float_toggle(sign(sample) == 1.0, max_lift_coefficient, abs(min_lift_coefficient))
+	var sample : float = lift_aoa_curve.sample_baked(remap(aoa, -PI, PI, lift_aoa_curve.min_domain, lift_aoa_curve.max_domain))
+	return sample * MathUtils.float_toggle(sign(sample) == 1.0, max_lift_coefficient / lift_aoa_curve.max_value, abs(min_lift_coefficient / lift_aoa_curve.min_value))
 
 func get_drag_coefficient(aoa : float) -> float:
-	return remap(drag_aoa_curve.sample_baked(aoa / PI / 2.0 + 0.5), 0, 1, min_drag_coefficient, max_drag_coefficient)
+	var sample : float = drag_aoa_curve.sample_baked(remap(aoa, -PI, PI, drag_aoa_curve.min_domain, drag_aoa_curve.max_domain))
+	return remap(sample, drag_aoa_curve.min_value, drag_aoa_curve.max_value, min_drag_coefficient, max_drag_coefficient)

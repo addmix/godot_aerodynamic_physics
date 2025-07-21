9bf479a5b38025f08d8ecc848462c83fca730e70
addmix <38119387+addmix@users.noreply.github.com>
2025-07-20 19:38:31 -0600

    Remove deprecated thruster utility class

--- demo/thruster.gd
+++
@@ -1,8 +0,0 @@
-extends Marker3D
-
-@export var enabled : bool = true
-@export var force : float = 100.0
-
-func _physics_process(delta : float) -> void:
-	if enabled:
-		get_parent().apply_force(-global_transform.basis.z * force, global_transform.basis * position)

a0db5660f6892c8b389cbeb57839a4fa5c49e36a
addmix <38119387+addmix@users.noreply.github.com>
2025-12-04 14:14:04 -0700

    Fixed buoyancy mesh calculations.

--- core/aero_influencer_3d/aero_buoyancy_3d/aero_buoyancy_mesh_3d.gd
+++ core/aero_influencer_3d/aero_buoyancy_3d/aero_buoyancy_mesh_3d.gd
@@ -15,6 +15,7 @@ var buoyancy_mesh_debug : MeshInstance3D
 #vertex buoyancy coefficients are the vertex normals, and the length is the "area" of the vertex.
 var vertex_positions : PackedVector3Array
 var vertex_buoyancy_coefficients : PackedVector3Array
+var vertex_sizes : PackedFloat64Array
 
 func _init() -> void:
 	super._init()
@@ -28,8 +29,12 @@ func _init() -> void:
 	debug_material.blend_mode = BaseMaterial3D.BLEND_MODE_ADD
 	debug_material.albedo_color.a = 0.2
 	buoyancy_mesh_debug.material_override = debug_material
-	buoyancy_mesh_debug.owner = owner
-	add_child(buoyancy_mesh_debug)
+	buoyancy_mesh_debug.visible = false
+
+func _ready() -> void:
+	super._ready()
+	
+	add_child(buoyancy_mesh_debug, INTERNAL_MODE_FRONT)
 
 func update_debug_visibility(_show_debug : bool = false) -> void:
 	super.update_debug_visibility(_show_debug)
@@ -99,10 +104,15 @@ func calculate_vertex_areas() -> void:
 	
 	var vertex_areas : PackedFloat64Array = []
 	var vertex_normals : PackedVector3Array = []
+	var _vertex_positions : PackedVector3Array = []
+	var _vertex_sizes : PackedFloat64Array = []
 	for vertex_index : int in vertex_triangle_affiliation_index_array.keys():
+		var vertex_position : Vector3 = sanitized_vertices[vertex_index]
 		
 		var area_sum : float = 0.0
 		var normal_sum : Vector3 = Vector3.ZERO
+		var position_sum : Vector3 = Vector3.ZERO
+		var distance_sum : float = 0.0
 		for triangle_index : int in vertex_triangle_affiliation_index_array[vertex_index]:
 			var triangle_index_offset : int = triangle_index * 3
 			#for semi-decent results, this area sum needs to be biased for triangle aspect ratio (obtuse/isosceles triangles)
@@ -116,21 +126,37 @@ func calculate_vertex_areas() -> void:
 			var side_ac : Vector3 = vert_c - vert_a
 			
 			var triangle_area : float = 0.5 * abs(side_ab.dot(side_ac)) / 3.0
-			var triangle_normal : Vector3 = side_ac.cross(side_ab).normalized()
 			area_sum += triangle_area
 			#normal sum should be biased using triangle area, where smaller triangles contribute less to the normal's direction.
+			var triangle_normal : Vector3 = side_ac.cross(side_ab).normalized()
+			#if the cross product isn't normalized, would the length of the result be something like twice the triangle's area?
 			normal_sum += triangle_normal * triangle_area # area can be encoded into normal length.
 			#the vertex position needs to be adjusted so that it is at the barycenter of the average triangles.
 			
-			#if the cross product isn't normalized, would the length of the result be something like twice the triangle's area?
+			var triangle_center : Vector3 = (vert_a + vert_b + vert_c) / 3.0
+			position_sum += triangle_center
 			
+			distance_sum += vertex_position.distance_to(triangle_center)
 			#maybe also calculate the average distance/offset of triangles?
-		#print(sanitized_vertices[vertex_index])
-		#print(normal_sum)
-		#print()
+		
 		
 		vertex_areas.append(area_sum)
 		vertex_normals.append(normal_sum)
+		#print(normal_sum)
+		position_sum /= vertex_triangle_affiliation_index_array[vertex_index].size()
+		_vertex_positions.append(position_sum)
+		#to visualize computed positions
+		#var mesh := CSGSphere3D.new()
+		#mesh.radius = 0.01
+		#mesh.position = position_sum
+		#add_child(mesh)
+		
+		#print(position_sum)
+		
+		distance_sum /= vertex_triangle_affiliation_index_array[vertex_index].size()
+		_vertex_sizes.append(distance_sum)
+		#print(distance_sum)
+		
 	
 	
 	#print(vertex_areas)
@@ -145,7 +171,7 @@ func calculate_vertex_areas() -> void:
 	var force_sum : Vector3 = Vector3.ZERO
 	var torque_sum : Vector3 = Vector3.ZERO
 	for vertex_index : int in sanitized_vertices.size():
-		var vertex_position : Vector3 = sanitized_vertices[vertex_index]
+		var vertex_position : Vector3 = _vertex_positions[vertex_index]
 		var vertex_buoyancy_factor : Vector3 = vertex_normals[vertex_index]
 		
 		force_sum += vertex_buoyancy_factor
@@ -155,8 +181,10 @@ func calculate_vertex_areas() -> void:
 	print(torque_sum)
 	
 	
-	vertex_positions = sanitized_vertices
+	vertex_positions = _vertex_positions#sanitized_vertices
 	vertex_buoyancy_coefficients = vertex_normals
+	
+	vertex_sizes = _vertex_sizes
 
 
 func calculate_buoyancy() -> void:
@@ -167,13 +195,32 @@ func calculate_buoyancy() -> void:
 		var vertex_position : Vector3 = global_basis * vertex_positions[vertex_index]
 		var global_vertex_position : Vector3 = aero_body.global_position + relative_position + vertex_position
 		var vertex_buoyancy_factor : Vector3 = global_basis * vertex_buoyancy_coefficients[vertex_index]
+		var vertex_radius : float = vertex_sizes[vertex_index]
+		
+		var vertex_density : float = aero_body.air_density 
+		
+		var vertex_velocity = world_air_velocity + -angular_velocity.cross(vertex_position) #this has to be negative because it's air velocity, not linear velocity
+		var vertex_airspeed : float = vertex_velocity.length()
+		var vertex_drag_direction : Vector3 = vertex_velocity.normalized()
 		
 		for atmosphere : AeroAtmosphere3D in aero_body.atmosphere_areas:
 			if not atmosphere.per_influencer_positioning:
 				# use precomputed center of pressure? yes, use precomputed center of pressure.
 				continue
 			
-			var force : Vector3 = Vector3(0, 1, 0) * atmosphere.density * aero_body.current_gravity.length() * min(atmosphere.get_distance_to_surface(global_vertex_position), 0.0) * vertex_buoyancy_factor
+			var distance_to_surface : float = atmosphere.get_distance_to_surface(global_vertex_position)
+			#this remap should be limited by the dot product of the vertex normal and the surface normal
+			vertex_density = clamp(remap(distance_to_surface, -vertex_radius, vertex_radius, atmosphere.density, aero_body.air_density), aero_body.air_density, atmosphere.density)
+			
+			var vertex_dynamic_pressure : float = 0.5 * vertex_density * vertex_airspeed * vertex_airspeed
+			
+			#this V3(0, 1, 0) term needs to be removed eventually, for proper buoyancy effects.
+			var force : Vector3 = vertex_density * aero_body.current_gravity.length() * min(distance_to_surface, 0.0) * vertex_buoyancy_factor# * Vector3(0, 1, 0)
+			#something in this drag calc causes a weird nose-up thing
+			
+			var lift_and_drag = vertex_buoyancy_factor.normalized() * vertex_dynamic_pressure * (vertex_drag_direction.dot(vertex_buoyancy_factor))
+			
+			force = force + lift_and_drag
 			
 			force_sum += force
 			#torque_sum += vertex_position.cross(force)
